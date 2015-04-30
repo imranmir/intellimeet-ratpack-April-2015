bc7cd28d4d3052e2443f1a33a4ce12915d659367
Mir Imran <imran@intelligrape.com>
2015-04-30 14:52:55 +0530

    Added jackson module example

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -6,11 +6,14 @@ import ratpack.groovy.template.TextTemplateModule
 import static ratpack.groovy.Groovy.groovyMarkupTemplate
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
+import ratpack.jackson.JacksonModule
+import static ratpack.jackson.Jackson.json
 
 ratpack {
   bindings {
     add MarkupTemplateModule
     add(TextTemplateModule) /*{ TextTemplateModule.Config config -> config.staticallyCompile = true }*/
+    add(new JacksonModule())
   }
 
   handlers {
@@ -29,6 +32,12 @@ ratpack {
     get("renderJSON"){
       Map map = [firstName: 'imran', lastName: 'Mir']
       render new JsonBuilder(map).toPrettyString()
+    }
+
+    get("renderJSONUsingJackson"){
+      Map map = [firstName: 'imran', lastName: 'Mir']
+      render json(map)
+
     }
 
     post ("personForm"){
